51beb49967585057f388384a67571412cd461026
Kayden12345 <72324679+Kayden12345@users.noreply.github.com>
2021-01-09 22:48:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bird;
+var pipes = [];
+ function preload(){
+  bird_Img = loadImage("images/bird_Img.png")
+  background_Img = loadImage("images/background_img.png")
+ }
+
+ function setup(){
+    canvas = createCanvas(displayWidth/2, displayHeight-30);
+    bird = new Bird;
+    pipes.push(new Pipe())
+    
+ }
+
+ function draw(){
+   background(random(0,255),random(0,255),random(0,255))  
+  
+
+   for (var i= pipes.length-1; i >= 0; i-- ){
+    pipes[i].show();
+    pipes [i].update();
+
+    if(pipes[i].hits(bird)){
+      console.log("hit!")
+    }
+
+    if(pipes[i].offscreen()){
+      pipes.splice(i,1);
+    }
+  }
+
+  bird.show();
+  bird.update();
+
+  if(frameCount % 30 == 0){
+    pipes.push(new Pipe())
+   }
+
+   translate(0,0,mouseX);
+
+}
+
+ function keyPressed() {
+   if(keyCode === 32){
+     bird.up();
+    //console.log(32)
+   }
+ }
+
+ 
